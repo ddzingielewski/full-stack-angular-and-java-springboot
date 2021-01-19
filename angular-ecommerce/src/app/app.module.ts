81cc25222109d332df90910ef857741d920e95f4
Dariusz Dzingielewski <ddzingielewski@sygnity.pl>
2021-01-19 15:25:10 +0100

    update cli

--- angular-ecommerce/src/app/app.module.ts
+++ angular-ecommerce/src/app/app.module.ts
@@ -40,7 +40,7 @@ const routes: Routes = [
     CheckoutComponent,
   ],
   imports: [
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
     BrowserModule,
     HttpClientModule,
     NgbModule,
